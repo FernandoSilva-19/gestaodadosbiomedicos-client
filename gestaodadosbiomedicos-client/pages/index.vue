19609d4e71681b15c3614b6a1e88c3f5615ce4ef
Fernando <fernandosilva2000@outlook.pt>
2021-12-21 18:36:22 +0000

    inicio frontend

---
+++ gestaodadosbiomedicos-client/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- easy components usage, already shipped with bootstrap css-->
+  <b-container>
+    <!-- try to remove :fields=”fields” to see the magic -->
+    <b-table striped over :items="utentes" :fields="fields" />
+  </b-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: ["username", "name", "email"],
+      utentes: [],
+    };
+  },
+  created() {
+    this.$axios.$get('/api/utentes').then((utentes) => { this.utentes = utentes })
+    //this.$axios.$get("http://localhost:8080/gestaodadosbiomedicos/api/utentes");
+  },
+};
+</script>
+<style></style>
