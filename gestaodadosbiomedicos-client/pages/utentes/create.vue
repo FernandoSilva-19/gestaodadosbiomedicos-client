4da0199e7516803d535d5e706c4140a055397712
Fernando <fernandosilva2000@outlook.pt>
2021-12-23 02:42:36 +0000

    utentes

--- gestaodadosbiomedicos-client/pages/utentes/create.vue
+++ gestaodadosbiomedicos-client/pages/utentes/create.vue
@@ -10,15 +10,12 @@
                placeholder="Introduz o teu nome" />
       <b-input ref="email" v-model.trim="email" type="email"
                :state="isEmailValid" required placeholder="Introduz o teu e-mail" />
-    <div>
       <b-select v-model="profissionalSaudeUsername" :options="profissionaisSaude"
-                :state="isProfissionalSaudeValid" required>
+                :state="isProfissionalSaudeValid" required value-field="ProfissionalSaudeUsername" text-field="ProfissionalSaudeUsername">
         <template v-slot:first>
-          <option :value="null" disabled>-- Selecione o profissional de saude --
-          </option>
+          <option :value="null" disabled>-- Selecione o profissional de saude --</option>
         </template>
       </b-select>
-    </div>
     <p class="text-danger" v-show="errorMsg">{{ errorMsg }}</p>
     <nuxt-link to="/utentes">Return</nuxt-link>
     <button type="reset">Reset</button>
