7ea0ad1a5f5a4052ec412f9a9d266583c89d2a95
Fernando <fernandosilva2000@outlook.pt>
2021-12-24 03:31:07 +0000

    lista de utentes dos funcionários

--- gestaodadosbiomedicos-client/pages/profissionaisSaude/_username/details.vue
+++ gestaodadosbiomedicos-client/pages/profissionaisSaude/_username/details.vue
@@ -1,11 +1,15 @@
 <template>
   <b-container>
-    <h4>Detalhes do Profisisonal de Saúde:</h4>
+    <h4>Detalhes do Profissional de Saúde:</h4>
     <p>Username: {{ profissionalSaude.username }}</p>
     <p>Name: {{ profissionalSaude.name }}</p>
     <p>Email: {{ profissionalSaude.email }}</p>
     <p>Tipo: {{ profissionalSaude.tipo }}</p>
-    <nuxt-link to="/">Back</nuxt-link>
+    <b>Utentes do {{ profissionalSaude.name }}:</b>
+      <template v-for="utente in utentes">
+        <li>{{utente.name}}</li>
+      </template>
+    <nuxt-link to="/profissionaisSaude">Back</nuxt-link>
   </b-container>
 </template>
 <script>
@@ -13,6 +17,7 @@ export default {
   data() {
     return {
       profissionalSaude: {},
+      utentes:{}
     };
   },
   computed: {
@@ -23,7 +28,10 @@ export default {
   created() {
     this.$axios
       .$get(`/api/profissionaisSaude/${this.username}`)
-      .then((profissionalSaude) => (this.profissionalSaude = profissionalSaude || {}))
+      .then((profissionalSaude) => (this.profissionalSaude = profissionalSaude || {})),
+      this.$axios
+      .$get(`/api/profissionaisSaude/${this.username}/utentes`)
+      .then((utentes) => (this.utentes = utentes || {}))
   },
 };
 </script>
