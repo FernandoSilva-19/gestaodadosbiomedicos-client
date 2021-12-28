ce455b854812cbf3245fcf007a395990105f8c5b
Fernando <fernandosilva2000@outlook.pt>
2021-12-28 23:04:36 +0000

    mudança nos detalhes do utente

--- gestaodadosbiomedicos-client/pages/utentes/_username/details.vue
+++ gestaodadosbiomedicos-client/pages/utentes/_username/details.vue
@@ -5,8 +5,26 @@
     <p>Name: {{ utente.name }}</p>
     <p>Email: {{ utente.email }}</p>
     <p>Profissional de Saúde: {{ utente.profissionalSaudeUsername }}</p>
+    <hr>
     <b>Dados biomedicos do {{ utente.name }}:</b>
     <b-table striped over :items="dadosBiomedicos" :fields="fields"> </b-table>
+    <hr>
+    <div><p>Altura: {{ dados.altura? dados.altura+" cm" : "DESCONHECIDO"}}</p>
+    <p>Peso: {{ dados.peso? dados.peso+" KG" : "DESCONHECIDO"}}</p>
+    <hr>
+    <div v-if="$auth.user.groups == 'Utente'">
+    <nuxt-link
+      :class="graphEnabled? 'btn btn-primary' : 'btn btn-danger'"
+      :event="graphEnabled? 'click' : ''"
+      :to="`/utentes/${$auth.user.sub}/datagraph`"
+    >Gráfico</nuxt-link>
+    <nuxt-link
+      :class="graphEnabled? 'btn btn-primary' : 'btn btn-danger'"
+      :event="graphEnabled? 'click' : ''"
+      :to="`/utentes/${$auth.user.sub}/datahistory`"
+    >Histórico</nuxt-link></div>
+    </div>
+    <hr>
     <div v-if="$auth.user.groups == 'Utente'">
       <nuxt-link to="/">Back</nuxt-link>
     </div>
@@ -22,6 +40,8 @@ export default {
       utente: {},
       dadosBiomedicos: {},
       fields: ["tipo", "unidadeMedicao", "limiteMinimo", "limiteMaximo", "valor"],
+       dados: {},
+      graphEnabled: true
     };
   },
   computed: {
@@ -38,6 +58,12 @@ export default {
         .then(
           (dadosBiomedicos) => (this.dadosBiomedicos = dadosBiomedicos || {})
         );
+        this.$axios
+      .$get(`/api/dadosutente/${this.$auth.user.sub}/latest`)
+      .then((dados) => this.dados = dados)
+      .catch((err) => {
+        if(err.response.status==404) this.graphEnabled = false
+      })
   },
 };
 </script>
