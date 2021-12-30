f041b67d7c2087a6a63bef803f23727e92cdbb9f
Rodrigo <rodrigofpintassilgo@gmail.com>
2021-12-30 21:43:45 +0000

    tabela dinamica com todos os dados

--- gestaodadosbiomedicos-client/pages/utentes/_username/addData.vue
+++ gestaodadosbiomedicos-client/pages/utentes/_username/addData.vue
@@ -2,9 +2,9 @@
 <div>
     <h1>Adicionar novos dados no utente</h1>
   <form @submit.prevent="add">
-     <b-input v-model.trim="altura" :state="isAlturaValid" required
+     <b-input v-model.trim="altura.valor" :state="isAlturaValid" required
                placeholder="Introduz a tua altura atual (medição: cm)" />
-      <b-input v-model="peso" :state="isPesoValid" required
+      <b-input v-model="peso.valor" :state="isPesoValid" required
                placeholder="Introduz o teu peso atual (medição: KG)" />
 
     <p class="text-danger" v-show="errorMsg">{{ errorMsg }}</p>
@@ -19,8 +19,8 @@ export default {
   data() {
     return {
       utenteUsername: "",
-      altura: null,
-      peso: null,
+      altura: {valor: "", minimo: "", maximo: ""},
+      peso: {valor: "", minimo: "", maximo: ""},
       errorMsg: false
     };
   },
@@ -28,32 +28,53 @@ export default {
           this.$axios
       .$get(`/api/utentes/${this.$auth.user.sub}`)
       .then((utente) => (this.utenteUsername = utente.username || {}))
+
+      // get limites
+      this.$axios.$get("/api/phenomenType/altura")
+      .then((altura) => {
+        this.altura.minimo = altura.minimo;
+        this.altura.maximo = altura.maximo;
+      })
+      this.$axios.$get("/api/phenomenType/peso")
+      .then((peso) => {
+        this.peso.minimo = peso.minimo;
+        this.peso.maximo = peso.maximo;
+      })
   },
   computed: {
     isAlturaValid () {
-      if (!this.altura) {
+      if (!this.altura.valor) {
         return null
       }
-      if (this.altura > 300 || this.altura < 100) { // estou a assumir que as pessoas têm pelo menos 100cm e menos de 300cm (epah se for preciso muda-se
-        return false                                // os bébes podem ter tipo 30cm e cenas assim e pode haver um mutante com mais de 3 metros sei lá hahaha)
+      if (this.altura.valor > this.altura.minimo || this.altura < this.altura.maximo) {
+        return false                             
       }
       return true
     },
     isPesoValid () {
-      if (!this.peso) {
+      if (!this.peso.valor) {
         return null
       }
-      if (this.peso > 400 || this.peso < 10) { // estou a assumir q um utente num minimo tem 10kg mas pode-se alterar um bebé pode ter menos de 10kg,
-        return false                           // assumo tbem q nao ha ninguem com mais de 400kg hahaha (nota: fui ver à net e a pessoa mais pesada alguma vez
-      }                                        // registada pesava 635kg cum crl haha
+      if (this.peso.valor > this.peso.minimo || this.peso < this.peso.maximo) {
+        return false                           
+      }                                       
       return true
     },
   },
   methods: {
     add() {
-      this.$axios.$post("/api/dadosutente", { // a testar, depois n vou passar assim o id nem a data como é obvio
-          peso: this.peso,
-          altura: this.altura,
+      this.$axios.$post("/api/observations", { 
+          valor: this.altura.valor,
+          phenomenTypeNome: "altura",
+          utenteUsername: this.utenteUsername
+        })
+        .catch(error => {
+          this.errorMsg = error.response.data
+        })
+
+      this.$axios.$post("/api/observations", { 
+          valor: this.peso.valor,
+          phenomenTypeNome: "peso",
           utenteUsername: this.utenteUsername
         })
         .then(() => {
